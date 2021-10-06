6e6b3323cc7bae145da11492d3fcf68023881a53
Roman <r.monahov2014@yandex.ru>
2021-10-06 12:30:35 +0300

    Настроил запуск сцены

---
+++ CombineTest/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CombineTest
+//
+//  Created by Роман on 06.10.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    private let toggle: UISwitch = {
+       let toggle = UISwitch()
+        return toggle
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
