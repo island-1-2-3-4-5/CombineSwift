03b117c5d3e2525687597f3d01970d30f1762674
Roman <r.monahov2014@yandex.ru>
2021-10-06 14:07:21 +0300

    Начал верстать интерфейс

---
+++ CombineTest/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  CombineTest
+//
+//  Created by Роман on 06.10.2021.
+//
+
+import UIKit
+
+
+struct Constants {
+    
+    struct Paddings {
+        static let toggleInserts = UIEdgeInsets(top: 48, left: 0, bottom: 0, right: 32)
+        static let titlleLabel = UIEdgeInsets(top: 48, left: 32, bottom: 0, right: 8)
+    }
+    
+    struct TextLabels {
+        static let titlleLabel = "Accept Terms?"
+        static let makePostButton = "Make a Post"
+    }
+    
+    
+}
--- CombineTest/MainViewController.swift
+++ CombineTest/MainViewController.swift
@@ -13,11 +13,24 @@ class MainViewController: UIViewController {
     //MARK: Values
     private let toggle: UISwitch = {
         let toggle = UISwitch()
-        toggle.translatesAutoresizingMaskIntoConstraints = false
         return toggle
     }()
     
+    private let titlleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        label.text = Constants.TextLabels.titlleLabel
+        return label
+    }()
     
+    
+    private let makePostButton: UIButton = {
+        let button = UIButton()
+        button.setTitle( Constants.TextLabels.makePostButton, for: .normal)
+        return button
+    }()
     
     
     //MARK: viewDidLoad
@@ -30,8 +43,24 @@ class MainViewController: UIViewController {
     
     //MARK: Functions
     private func setViews() {
-        view.addSubview(toggle)
+        view.backgroundColor = .white
+        view.addSubviews(toggle,
+                         titlleLabel,
+                         makePostButton)
+        layoutViews()
+    }
+    
+    fileprivate func layoutViews() {
+        toggle.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                      leading: nil,
+                      bottom: nil,
+                      trailing: view.trailingAnchor,
+                      padding: Constants.Paddings.toggleInserts)
         
+        titlleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                           leading: view.leadingAnchor,
+                           bottom: nil,
+                           trailing: toggle.leadingAnchor, padding: Constants.Paddings.titlleLabel)
     }
     
 }
